ea12e9f9c888f97e7dace74d88cf6027a866dfe1
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-06-03 20:14:56 +0300

    add logic to BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,63 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->input('search');
+        $books = Book::where('name', 'like', "%$search%")->orWhere('author', 'like', "%$search%")->paginate(10);
+
+        return view('books.index', compact('books'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('books.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'author' => 'required',
+            // Add validation rules for other fields
+        ]);
+
+        Book::create($validatedData);
+
+        return redirect()->route('books.index')->with('success', 'Book created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Book $book)
     {
-        //
+        return view('books.show', compact('book'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Book $book)
     {
-        //
+        return view('books.edit', compact('book'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Book $book)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'author' => 'required',
+            // Add validation rules for other fields
+        ]);
+
+        $book->update($validatedData);
+
+        return redirect()->route('books.index')->with('success', 'Book updated successfully.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Book $book)
     {
-        //
+        $book->delete();
+
+        return redirect()->route('books.index')->with('success', 'Book deleted successfully.');
+    }
+
+    public function buy(Book $book): RedirectResponse
+    {
+        $user = Auth::user();
+        $user->budget = $book->price;
+        $user->save();
+
+        return redirect()->route('books.show', $book)->with('success', 'Book purchased successfully.');
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $price
+ */
 class Book extends Model
 {
     use HasFactory;

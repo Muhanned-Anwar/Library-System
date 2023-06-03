d389132dc1b97db244986e410680c8111a1631b2
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-06-03 20:16:59 +0300

    show all books

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Books</h1>
+    <form action="{{ route('books.index') }}" method="GET">
+        <input type="text" name="search" placeholder="Search...">
+        <button type="submit">Search</button>
+    </form>
+
+    <!-- Book listing table -->
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Author</th>
+            <!-- Add more columns -->
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($books as $book)
+            <tr>
+                <td>{{ $book->name }}</td>
+                <td>{{ $book->author }}</td>
+                <!-- Add more columns -->
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
+    {{ $books->links() }}
+@endsection

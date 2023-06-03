d2e9006028ccdef25579a3e0c1e66d3e7c9e3055
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-06-03 21:23:46 +0300

    add route

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/books/{book}/buy', [BookController::class, 'buy'])->name('books.buy');
 });
 
 
 Route::get('/', [BookController::class, 'index'])->name('home');
 Route::resource('books', BookController::class);
-Route::post('/books/{book}/buy', [BookController::class, 'buy'])->name('books.buy');
 
 require __DIR__.'/auth.php';
